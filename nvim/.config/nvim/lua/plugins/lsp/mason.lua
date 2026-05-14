f01e544aafd1adc018c8c15636c255be84252ed4
kadam-x <sn33zyhub@gmail.com>
2026-05-14 22:06:53 +0200

    new lsp config

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,49 +1,149 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "html",
-        "cssls",
-        "lua_ls",
-        "pyright",
-        "clangd",         
-        "rust_analyzer",  
-        "astro",          
-        "eslint",
-      },
-    },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
-          },
-        },
-      },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        "prettier",      
-        "stylua",        
-        "isort",         
-        "black",         
-        "clang-format",  
-        "pylint",
-        "eslint_d",
-      },
-    },
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = {
+				"html",
+				"cssls",
+				"lua_ls",
+				"clangd",
+				"rust_analyzer",
+				"astro",
+				"eslint",
+				"pyrefly",
+				"gopls",
+			},
+		},
+		config = function(_, opts)
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+			require("mason-lspconfig").setup(opts)
+
+			vim.lsp.config("*", {})
+
+			local servers = {
+				"html",
+				"cssls",
+				"lua_ls",
+				"clangd",
+				"rust_analyzer",
+				"astro",
+				"eslint",
+				"gopls",
+			}
+			for _, server in ipairs(servers) do
+				vim.lsp.enable(server)
+			end
+
+			vim.lsp.config("pyrefly", {
+				cmd = { "pyrefly", "lsp" },
+				filetypes = { "python" },
+				root_markers = {
+					"pyrefly.toml",
+					"pyproject.toml",
+					"setup.py",
+					"setup.cfg",
+					"requirements.txt",
+					"Pipfile",
+					".git",
+				},
+			})
+			vim.lsp.enable("pyrefly")
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local o = { buffer = args.buf }
+
+					-- Standard binds
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", o, { desc = "Hover docs" }))
+					vim.keymap.set(
+						"n",
+						"gd",
+						vim.lsp.buf.definition,
+						vim.tbl_extend("force", o, { desc = "Go to definition" })
+					)
+					vim.keymap.set(
+						"n",
+						"gr",
+						vim.lsp.buf.references,
+						vim.tbl_extend("force", o, { desc = "Go to references" })
+					)
+					vim.keymap.set(
+						"i",
+						"<C-k>",
+						vim.lsp.buf.signature_help,
+						vim.tbl_extend("force", o, { desc = "Signature help" })
+					)
+
+					-- <leader>l LSP group
+					vim.keymap.set(
+						"n",
+						"<leader>ln",
+						vim.lsp.buf.rename,
+						vim.tbl_extend("force", o, { desc = "Rename symbol" })
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>la",
+						vim.lsp.buf.code_action,
+						vim.tbl_extend("force", o, { desc = "Code action" })
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>ld",
+						vim.lsp.buf.declaration,
+						vim.tbl_extend("force", o, { desc = "Go to declaration" })
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>li",
+						vim.lsp.buf.implementation,
+						vim.tbl_extend("force", o, { desc = "Go to implementation" })
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>lr",
+						vim.lsp.buf.references,
+						vim.tbl_extend("force", o, { desc = "Go to references" })
+					)
+				end,
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"clang-format",
+				"eslint_d",
+			},
+		},
+		dependencies = { "williamboman/mason.nvim" },
+	},
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,52 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = "VeryLazy",
-    branch = "master",
-    build = ":TSUpdate",
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    opts = {
-        ensure_installed = {
-            "bash", "c", "diff", "html", "lua", "luadoc",
-            "markdown", "markdown_inline", "query", "vim", "vimdoc",
-            "rust", "regex", "python", "nix", "astro", "cpp",
-            "php", "blade",
-        },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { "ruby" },
-        },
-        indent = { enable = true, disable = { "ruby" } },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<M-space>",
-                node_incremental = "<M-space>",
-                scope_incremental = false,
-                node_decremental = "<Backspace>",
-            },
-        },
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
-                    ["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
-                    ["ac"] = { query = "@class.outer",    desc = "Select outer part of a class region" },
-                    ["ic"] = { query = "@class.inner",    desc = "Select inner part of a class region" },
-                    ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>xs"] = { query = "@parameter.inner", desc = "Swap with next" },
-                },
-                swap_previous = {
-                    ["<leader>xS"] = { query = "@parameter.inner", desc = "Swap with previous" },
-                },
-            },
-        },
-    },
+	"nvim-treesitter/nvim-treesitter",
+	version = false,
+	branch = "main",
+	event = "VeryLazy",
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"rust",
+			"regex",
+			"python",
+			"nix",
+			"astro",
+			"cpp",
+			"php",
+			"blade",
+		},
+		auto_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		vim.treesitter.language.setup = vim.treesitter.language.setup or function() end
+		require("nvim-treesitter").setup(opts)
+	end,
 }
