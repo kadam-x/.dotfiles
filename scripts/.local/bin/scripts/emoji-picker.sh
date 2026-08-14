21d8e804e7edefd7089e4017df671b7b1cbb62f2
kadam-x <sn33zyhub@gmail.com>
2026-08-14 15:45:48 +0200

    fix(emoji-picker): opts removed redundancy

--- scripts/.local/bin/scripts/emoji-picker.sh
+++ scripts/.local/bin/scripts/emoji-picker.sh
@@ -2,7 +2,7 @@
 
 EMOJI_FILE="$HOME/.local/bin/scripts/emoji_list.txt"
 
-chosen=$(cut -d';' -f1 "$EMOJI_FILE" | bemenu $bemenu_opts | awk '{print $1}')
+chosen=$(bemenu -p 'emoji:' < "$EMOJI_FILE" | awk '{print $1}')
 
 if [ -n "$chosen" ]; then
     printf "%s" "$chosen" | wl-copy
