854ebf255700fc78a661661406db92e72b768a95
kadam-x <sn33zyhub@gmail.com>
2026-08-21 18:04:50 +0200

    nvim mini tabline, sway gaps reduce

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,5 +1,5 @@
 vim.opt.termguicolors = true
-vim.opt.showtabline = 0
+vim.opt.showtabline = 3
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.showmode = false
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -72,9 +72,6 @@ return {
 				{ win = { position = "float", width = 0.95, height = 0.65, border = "single" } }
 			)
 		end, { desc = "Toggle terminal" })
-		vim.keymap.set("n", "<tab>", function()
-			Snacks.picker.buffers()
-		end, { desc = "Find Buffers" })
 		vim.keymap.set("n", "<leader>ff", function()
 			Snacks.picker.files()
 		end, { desc = "Find Files" })
