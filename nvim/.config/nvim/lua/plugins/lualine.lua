6aa496227f9bc9b284e13806c5ce11b8ed44bbb5
kadam-x <sn33zyhub@gmail.com>
2026-08-21 14:51:39 +0200

    zathura config, yazi keybind remaps zathura for pdf viewing

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -13,8 +13,8 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = {
-					{ "filename", symbols = { modified = " +", readonly = " -", unnamed = "" } },
 					{ "branch", icon = "" },
+					{ "filename", symbols = { modified = " +", readonly = " -", unnamed = "" } },
 				},
 				lualine_c = {},
 				lualine_x = {
@@ -22,23 +22,14 @@ return {
 						function()
 							if #vim.lsp.get_clients({ bufnr = 0 }) > 0 then
 								local devicons = require("nvim-web-devicons")
-								local icon =
-									devicons.get_icon(vim.fn.expand("%:t"), vim.bo.filetype, { default = true })
-								return (icon or "") .. " " .. vim.bo.filetype
+								return devicons.get_icon(vim.fn.expand("%:t"), vim.bo.filetype, { default = true })
+									or ""
 							end
 							return ""
 						end,
 					},
-					{
-						function()
-							local count = #vim.tbl_filter(function(bufnr)
-								return vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buflisted
-							end, vim.api.nvim_list_bufs())
-							return "bufs: " .. count
-						end,
-					},
 				},
-				lualine_y = {},
+				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
 			inactive_sections = {
