20b553ba56a49d6d11cea11bf6e9fa4a8a89c033
kadam-x <sn33zyhub@gmail.com>
2026-08-23 21:07:08 +0200

    move to dwl

--- scripts/.local/bin/scripts/emoji-picker.sh
+++ scripts/.local/bin/scripts/emoji-picker.sh
@@ -2,7 +2,7 @@
 
 EMOJI_FILE="$HOME/.local/bin/scripts/emoji_list.txt"
 
-chosen=$(bemenu -p 'emoji:' < "$EMOJI_FILE" | awk '{print $1}')
+chosen=$(eval "bemenu -p 'emoji:' $BEMENU_OPTS" < "$EMOJI_FILE" | awk '{print $1}')
 
 if [ -n "$chosen" ]; then
     printf "%s" "$chosen" | wl-copy
--- scripts/.local/bin/scripts/wallpaper-picker.sh
+++ scripts/.local/bin/scripts/wallpaper-picker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 wp_dir="$HOME/Pictures/wallpapers"
 
-selected=$(find "$wp_dir" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -printf "%f\n" | sort | bemenu -p "wallpaper:")
+selected=$(find "$wp_dir" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -printf "%f\n" | sort | eval "bemenu -p 'wallpaper:' $BEMENU_OPTS")
 
 [ -z "$selected" ] && exit 0
 
