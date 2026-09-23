a9abfa63614ef0fef69749b91ec50fa13c2e4fb3
kadam-x <contact.jumbo009@passinbox.com>
2026-09-23 18:45:27 +0200

    nvim whichkey + bg color fix

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,13 @@
 return {
-	"kyazdani42/blue-moon",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.opt.termguicolors = true
-		vim.cmd("colorscheme blue-moon")
-	end,
+    "kyazdani42/blue-moon",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme("blue-moon")
+
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    end,
 }
+
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -5,18 +5,38 @@ return {
 		require("which-key").setup({
 			delay = 0,
 			preset = "helix",
+
+			icons = {
+				group = "",
+			},
+
 			triggers = {
 				{ "<leader>", mode = "n" },
 				{ "s", mode = "n" },
 			},
+
 			spec = {
-				{ "<leader>s", group = "Surround", icon = "" },
+				{ "<leader>c", group = "onform", icon = "C" },
+				{ "<leader>e", group = "xplorer", icon = "E" },
+				{ "<leader>r", group = "eplace", icon = "R" },
+				{ "<leader>f", group = "ind", icon = "F" },
+				{ "<leader>l", group = "sp", icon = "L" },
+				{ "<leader>s", group = "urround", icon = "S" },
+
+				{ "<leader>fb", group = "uffer Grep", icon = "B" },
+				{ "<leader>fc", group = "onfig Files", icon = "C" },
+				{ "<leader>fd", group = "iagnostics", icon = "D" },
+				{ "<leader>ff", group = "ind Files", icon = "F" },
+				{ "<leader>fg", group = "rep Files", icon = "G" },
+				{ "<leader>fh", group = "elp Tags", icon = "H" },
+				{ "<leader>fk", group = "eymaps", icon = "K" },
+				{ "<leader>fp", group = "ath to clipboard", icon = "P" },
+				{ "<leader>fr", group = "ecent Files", icon = "R" },
+				{ "<leader>ft", group = "ODO List", icon = "T" },
+				{ "<leader>fw", group = "ord Find", icon = "W" },
+
 				{ "<leader>fy", hidden = true },
-				{ "<leader>e", icon = "󰇥" },
-				{ "<leader>r", icon = "󰛔" },
 				{ "<leader>E", hidden = true },
-				{ "<leader>l", group = "Lsp" },
-				{ "<leader>f", group = "Find" },
 				{ "<leader>d", hidden = true },
 				{ "<leader>lwa", hidden = true },
 				{ "<leader>lwr", hidden = true },
