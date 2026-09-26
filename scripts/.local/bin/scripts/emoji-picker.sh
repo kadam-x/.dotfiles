d16b78f2e102fc33362b671b53a6397bb221df65
kadam-x <contact.jumbo009@passinbox.com>
2026-09-26 20:09:01 +0200

    -bemenu, emoji script, + tofi

--- scripts/.local/bin/scripts/emoji-picker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-EMOJI_FILE="$HOME/.local/bin/scripts/emoji_list.txt"
-
-chosen=$(eval "bemenu -p 'emoji:' $BEMENU_OPTS" < "$EMOJI_FILE" | awk '{print $1}')
-
-if [ -n "$chosen" ]; then
-    printf "%s" "$chosen" | wl-copy
-fi
--- scripts/.local/bin/scripts/wallpaper-picker.sh
+++ scripts/.local/bin/scripts/wallpaper-picker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+
 wp_dir="$HOME/Pictures/wallpapers"
 
-selected=$(find "$wp_dir" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -printf "%f\n" | sort | eval "bemenu -p 'wallpaper:' $BEMENU_OPTS")
+selected=$(find "$wp_dir" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -printf "%f\n" | sort | tofi --prompt-text "wallpaper: ")
 
 [ -z "$selected" ] && exit 0
 
