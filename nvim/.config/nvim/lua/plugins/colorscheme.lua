ddc8b7abf433fc1c46027457dbd1c7a3de23a5c5
kadam-x <contact.jumbo009@passinbox.com>
2026-09-26 16:52:54 +0200

    vague colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,8 @@
 return {
-    "kyazdani42/blue-moon",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme("blue-moon")
-
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-    end,
+	"vague26/vague.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("vague")
+	end,
 }
-
